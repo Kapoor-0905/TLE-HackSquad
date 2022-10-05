4a509300f0bd0381dc21b097f1fd78a5bb1d7d38
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2022-10-05 10:20:38 +0530

    adding node in linked list
    added node infront and at  position

---
+++ linked_list/.cpp
@@ -0,0 +1,34 @@
+struct node {
+    int data;
+    struct node *next;
+}
+
+void push(Node** head_ref, int new_data)
+{
+
+    Node* new_node = new Node();
+
+    new_node->data = new_data;
+
+    new_node->next = (*head_ref);
+
+    (*head_ref) = new_node;
+}
+
+void insertmid(Node* prev_node, int new_data)
+{
+
+    if (prev_node == NULL) {
+        cout << "The given previous node cannot be NULL";
+        return;
+    }
+ 
+    Node* new_node = new Node();
+ 
+    new_node->data = new_data;
+ 
+    new_node->next = prev_node->next;
+ 
+    prev_node->next = new_node;
+}
+
